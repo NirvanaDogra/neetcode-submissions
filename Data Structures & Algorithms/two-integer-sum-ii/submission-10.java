657e10d2bafbc76ab5d0f96ce3a8a7a1694d55f0
Nirvana Dogra <nirvanadogra004@gmail.com>
2026-08-01 10:50:36 -0700

    Add: two-integer-sum-ii - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-10.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int leftPtr = 0;
+        int rightPtr = numbers.length - 1;
+
+        while(leftPtr < rightPtr) {
+            int sum = numbers[leftPtr] + numbers[rightPtr];
+            if (sum == target) {
+                return new int[] { leftPtr+1, rightPtr+1};
+            } 
+            if (sum < target) {
+                leftPtr++;
+            } else {
+                rightPtr--;
+            }
+        }
+        return null;
+    }
+}
