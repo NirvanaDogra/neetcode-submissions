531c0eec2a67415b802ceef2654a0c8f5614426c
Nirvana Dogra <nirvanadogra004@gmail.com>
2026-08-03 13:38:28 -0700

    Add: evaluate-reverse-polish-notation - submission-8

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-8.java
@@ -0,0 +1,39 @@
+class Solution {
+    Stack<Integer> evalStack = new Stack<Integer>();
+    public int evalRPN(String[] tokens) {
+        String operators = "+*-/";
+        for(String t: tokens) {
+            switch(t) {
+                case "+": {
+                    int second = evalStack.pop();
+                    int first = evalStack.pop();
+                    evalStack.push(first+second);
+                    break;
+                }
+                case "*": {
+                    int second = evalStack.pop();
+                    int first = evalStack.pop();
+                    evalStack.push(first*second);
+                    break;
+                }
+                case "-": {
+                    int second = evalStack.pop();
+                    int first = evalStack.pop();
+                    evalStack.push(first-second);
+                    break;
+                }
+                case "/": {
+                    int second = evalStack.pop();
+                    int first = evalStack.pop();
+                    evalStack.push(first/second);
+                    break;
+                }
+                default: {
+                    evalStack.push(Integer.parseInt(t));
+                    break;
+                }
+            }
+        }
+        return evalStack.peek();
+    }
+}
